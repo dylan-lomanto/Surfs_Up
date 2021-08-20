503c4f2ad9bf786f0c09ad13c49339dca60045e2
Dylan Lomanto <dylan.p.lomanto@gmail.com>
2021-08-19 20:58:58 -0700

    flask 2

--- app.py
+++ app.py
@@ -1,5 +1,49 @@
-from flask import Flask
+#import dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+#Set up database
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+
+#reflect the database into SQLAlchemy
+Base.prepare(engine, reflect=True)
+
+#Save referencews to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#Create session link from Python to database
+session = Session(engine)
+
+#Define Flask app
 app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return 'Hello world'
+
+#Create Welcome route (root)
+@app.route("/")
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+
+
+# Create Precipitation route
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
